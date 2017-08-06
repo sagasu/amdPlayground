ca3f2e62a4fb218f7aa29885d29516f893a5295e
sagasu <sagasu@gmail.com>
2017-08-06 23:10:18 +0200

    playing with let statement

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict'
-console.log(productId)
+console.log(productId); //undefined will be displayed
 var productId = 12;
+
+
+console.log(productId2); //error will be thrown
+let productId2 = 12;
+
+let productId3;
+console.log(productId3); //undefined
