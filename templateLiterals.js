4dc6f4fe448e19caf3d5962506764797445c38c8
sagasu <sagasu@gmail.com>
2017-08-07 22:01:50 +0200

    abusing more template literals

--- templateLiterals.js
+++ templateLiterals.js
@@ -11,3 +11,15 @@ processMe `bar`;
 
 
 processMe(`bar ${invoiceNumber}`); //bar 123   ---not it will be showed as a string
+
+
+var buzz =12;
+function digDeep(head, ...tail){
+  console.log(head);
+  console.log(tail);
+  //["foo: ", " bar ", "", raw: Array(3)]
+  // ["123", 12]
+  // This is interesting behaviour, because interpolation of such string happened inside method
+}
+
+digDeep `foo: ${invoiceNumber} bar ${buzz}`
