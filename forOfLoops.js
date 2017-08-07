104546f39429cb87b1bcfa451a70c15f868f230d
sagasu <sagasu@gmail.com>
2017-08-07 21:25:55 +0200

    checking iterators for strings and empty arrays

--- forOfLoops.js
+++ forOfLoops.js
@@ -3,3 +3,12 @@ var categories = ['foo', 'bar', 'buzz'];
 for (var item of categories) {
   console.log(item); //foo, bar, buzz
 }
+
+var categories = [,,];
+for (var item of categories) {
+  console.log(item); //undefined, undefined    --- only 2 undefined, because in js array ends with undefined
+}
+
+for (var item of "abcd") {
+  console.log(item); //a,b,c,d
+}
