74dfbce4e25a716b42365116f9a759edcc68d59f
sagasu <sagasu@gmail.com>
2017-08-07 17:57:44 +0200

    playing with a scope of arrow functions

--- arrow.js
+++ arrow.js
@@ -1,3 +1,29 @@
 'use strict';
 var getPrice = () => 5.99;
 console.log(typeof getPrice); //function
+
+var invoice = {
+  number: 123,
+  process: function () {
+    console.log(this); // object{number: 123 ...}
+  }
+};
+invoice.process();
+
+
+var invoice = {
+  number: 123,
+  process: () => {
+    console.log(this); // window...
+  }
+};
+invoice.process();
+
+var invoice = {
+  number: 123,
+  process: function() {
+    // because we are working in a scope of a funciton, and not window any more
+    return () => console.log(this.number); // 123
+  }
+};
+invoice.process()();
