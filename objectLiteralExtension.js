14f0fe5c65f1a970a1994b4b37c3a0c78cc4f7e7
sagasu <sagasu@gmail.com>
2017-08-07 20:42:40 +0200

    abusing dynamic fields- literals

--- objectLiteralExtension.js
+++ objectLiteralExtension.js
@@ -5,3 +5,19 @@ var product = {
   quantity
 };
 console.log(product); //{price: 33, quantity: 30}
+
+var product = {
+  price: 10,
+  quantity: 20,
+  "foo bar"() {
+    return this.price * this.quantity;
+  }
+}
+console.log(product["foo bar"]()); //200
+
+
+var myName = "foobar"
+var product = {
+  [myName]: price
+}
+console.log(product);// {foobar: 33}
